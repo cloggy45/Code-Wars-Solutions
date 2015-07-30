851aafdd79e3667d3f62de57806a84c5606af011
Paul Mcloughlin <cloggy45@googlemail.com>
2015-07-30 18:51:27 +0100

    Completed kata

--- Javascript/Class-Classes.js
+++ Javascript/Class-Classes.js
@@ -10,3 +10,18 @@
     Your task is to complete a class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number,
     complete the get info property and getInfo method which should return 'Johns age is 34'
  */
+
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+    get info() {
+        return this.getInfo()
+    }
+
+    getInfo() {
+        return this._name + 's age is ' + this._age;
+    }
+}
