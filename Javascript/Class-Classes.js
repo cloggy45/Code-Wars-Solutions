787baf4b6bbf4eb07f233026e5ab4a39b8e7985a
Paul Mcloughlin <cloggy45@googlemail.com>
2015-07-30 18:24:06 +0100

    Adding description of kata

---
+++ Javascript/Class-Classes.js
@@ -0,0 +1,12 @@
+/**
+ * Created by drderp45 on 30/07/2015.
+ */
+
+/*
+    Classy Classes
+    This kata is mainly aimed at the new JS ES6 Update introducing classes
+
+    Task
+    Your task is to complete a class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number,
+    complete the get info property and getInfo method which should return 'Johns age is 34'
+ */
