cbc9708b7dd29951b691967330c666c0689b363f
Paul Mcloughlin <cloggy45@googlemail.com>
2015-08-01 22:55:14 +0100

    Added kata description

---
+++ Javascript/Implementing-a-Queue.js
@@ -0,0 +1,5 @@
+/*
+ In this problem, we are going to be implementing our own enqueue, dequeue, and size methods for the queue constructor we are creating,
+ so we should be able to create new instances of the Queue.
+ */
+
