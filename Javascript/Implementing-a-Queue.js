14325500fe23e773414b822a7c235a709d022bae
Paul Mcloughlin <cloggy45@googlemail.com>
2015-08-01 23:02:21 +0100

    Finished Kata

--- Javascript/Implementing-a-Queue.js
+++ Javascript/Implementing-a-Queue.js
@@ -3,3 +3,21 @@
  so we should be able to create new instances of the Queue.
  */
 
+var Queue = function() {
+    // implement your Queue constructor here
+    this._queue = [];
+};
+
+Queue.prototype.enqueue = function(item) {
+    // implement enqueue
+    this._queue.push(item);
+};
+
+Queue.prototype.dequeue = function() {
+    // implement dequeue
+    this._queue.unshift();
+};
+
+Queue.prototype.size = function() {
+    return this._queue.length; // the size of the queue
+};
