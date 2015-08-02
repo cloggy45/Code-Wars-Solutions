b9b46a2a87d38b6bb1ba7b4e7962a4cab5e49072
Paul Mcloughlin <cloggy45@googlemail.com>
2015-08-02 20:46:47 +0100

    Added tests

--- Javascript/Implementing-a-Queue/tests/Implementing-a-Queue-Tests.js
+++ Javascript/Implementing-a-Queue/tests/Implementing-a-Queue-Tests.js
@@ -1,17 +1,26 @@
-QUnit.test('Queue object exists', function (assert) {
-    var queueObj = new Queue();
-    assert.deepEqual(queueObj, new Queue(), "Object is the same")
-});
-
-QUnit.module('Enqueue method', {
+QUnit.module('Queue Object', {
     beforeEach: function () {
         this._queueObject = new Queue();
+        this._queueObject.enqueue('alpha'); // First
+        this._queueObject.enqueue('beta'); // Second
+        this._queueObject.enqueue('charlie'); // Third
     }
 });
 
 QUnit.test('Enqueue method adds items to array', function (assert) {
-    this._queueObject.enqueue('alpha');
-    this._queueObject.enqueue('beta');
-    this._queueObject.enqueue('charlie');
-    assert.equal(this._queueObject._queue.length, 3, "Queue array contains 3 items");
+
+    this._queueObject.enqueue('delta');
+    assert.equal(this._queueObject._queue.length, 4, "Sucessfully added elements to queue");
 });
+
+QUnit.test('Dequeue method removes element from front of queue', function (assert) {
+
+    var result = this._queueObject.dequeue();
+    assert.equal(result, 'alpha', 'Removed first element from queue');
+});
+
+QUnit.test('Size method returns size of queue', function (assert) {
+    var size = this._queueObject.size();
+    assert.equal(size, 3, 'Size method returns correct size: 3');
+});
+
