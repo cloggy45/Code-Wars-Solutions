445ee507f06eccfc2f0377d25a8efde336b05dc8
cloggy45 <cloggy45@googlemail.com>
2015-08-08 20:41:18 +0100

    Actually finished the kata properly

--- Javascript/Russian-Postal-Code-Checker.js
+++ Javascript/Russian-Postal-Code-Checker.js
@@ -1,6 +1,6 @@
 // http://www.codewars.com/kata/552e45cc30b0dbd01100001a/train/javascript
 
 function zipvalidate(postcode){
-    return /^([^05789])(\d{5})$/.test(postcode);
+    return /^([^05789\W\D])(\d{5})$/.test(postcode);
 }
 
