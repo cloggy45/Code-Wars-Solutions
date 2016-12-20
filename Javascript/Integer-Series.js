2f406cb6d0b83c38433a5d84a5f07641deab428d
drderp <cloggy45@googlemail.com>
2016-12-20 20:47:42 +0000

    Finished solution

---
+++ Javascript/Integer-Series.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Write a function that accepts two arguments (start, end) and generates an array containing integers
+
+
+function generateIntegers(start, end) {
+	var container = [];
+	for(let curNum = start;curNum<=end;curNum++) {
+		container.push(x);	
+	}
+	return container;
+}
+
+
